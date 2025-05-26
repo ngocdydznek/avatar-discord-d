09baa3911d505c7b1a86b3adc8f65c87da312e97
ngocdydznek <168576578+ngocdydznek@users.noreply.github.com>
2025-05-26 20:09:37 +0700

    index.js

--- index.js
+++ index.js
@@ -153,9 +153,17 @@ client.on(Events.InteractionCreate, async (interaction) => {
   }
 });
 
+onst express = require('express');
+const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Web server is listening on port ${PORT}`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
